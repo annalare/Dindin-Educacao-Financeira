75323f70d35e94672bc1fac30b86064ba833cc54
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-06-16 16:46:10 -0300

    js

--- index.js
+++ index.js
@@ -11,4 +11,25 @@ if (local === "/index.html") {
 
   let titulos = document.querySelectorAll(".titulo");
   titulos.forEach((a) => (a.style.textTransform = "uppercase"));
+
+  document
+    .querySelector("#posts")
+    .setAttribute("href", "./View/maisconteudo.html");
+}
+
+if (local === "/View/contato.html") {
+  let form = document.querySelector("form");
+  form.setAttribute("action", "./sucesso.html");
+
+  let textarea = document.querySelector("textarea");
+  let telefone = document.createElement("input");
+  telefone.setAttribute("type", "tel");
+  telefone.setAttribute("class", "dados");
+  telefone.setAttribute("placeholder", "telefone");
+  form.insertBefore(telefone, textarea);
+
+  textarea.setAttribute("style", "box-sizing:border-box");
+
+  let botao = document.querySelector("#botao");
+  botao.setAttribute("style", "width:100%");
 }
